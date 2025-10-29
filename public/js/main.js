c711baa0efd78f1f1e2affc6065e9355cac0b925
Nitin Kaushal <nitinkaushaldmi1848@gmail.com>
2025-10-29 14:34:46 +0530

    UI changes done and typing animation introduced

--- public/js/main.js
+++ public/js/main.js
@@ -61,3 +61,39 @@
     "use strict";
 
 }) ();
+
+
+//Typing Animation Hero-Head
+const phrases = [
+  "Eye 'Can'cers!",
+  "I 'Can'!"
+];
+
+const typedText = document.querySelector(".typed-text");
+
+let phraseIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+  const currentPhrase = phrases[phraseIndex];
+  const currentText = currentPhrase.substring(0, charIndex);
+
+  typedText.textContent = currentText;
+
+  if (!isDeleting && charIndex < currentPhrase.length) {
+    charIndex++;
+    setTimeout(type, 100);
+  } else if (isDeleting && charIndex > 0) {
+    charIndex--;
+    setTimeout(type, 60);
+  } else {
+    isDeleting = !isDeleting;
+    if (!isDeleting) {
+      phraseIndex = (phraseIndex + 1) % phrases.length;
+    }
+    setTimeout(type, 1000);
+  }
+}
+
+type();
